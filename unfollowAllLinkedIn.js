159b5a87a2b72410973a73ba4b35aa2b33fb3c23
Anubhab Patnaik <anubhabr50@gmail.com>
2023-04-28 15:11:36 +0530

    Update unfollowAllLinkedIn.js

--- unfollowAllLinkedIn.js
+++ unfollowAllLinkedIn.js
@@ -1,45 +1,24 @@
 (() => {
-
  async function unfollowAll() {
-
   const buttons = document.querySelectorAll('button.follow.is-following') || [];
 
-		
-
   for (const button of buttons) {
-
    const name = button.parentElement.querySelector('.follows-recommendation-card__name',).innerText;
-
    console.log('Unfollowed ', name);
 
-			
-
    window.scrollTo(0, button.offsetTop - 260);
-
    button.click();
-
-
-
    await new Promise((resolve) => setTimeout(resolve, 100));
-
   }
-
  }
 
  async function runUnfollowAll() {
-
   await unfollowAll();
-
   window.scrollTo(0, document.body.scrollHeight);
-
   await new Promise((resolve) => setTimeout(resolve, 2000));
-
   const buttons = document.querySelectorAll('button.follow.is-following') || []; 
 
   if (buttons.length) runUnfollowAll();
-
  }
-
  runUnfollowAll();
-
 })();
